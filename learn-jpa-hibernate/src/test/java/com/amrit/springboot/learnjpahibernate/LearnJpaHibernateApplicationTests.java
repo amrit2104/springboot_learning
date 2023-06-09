c1da0233baa753dedeee7ffbdc8d559f4c2e87dd
Amrit Gaurav Rath <NH1965@nslhub.in>
2023-06-09 14:32:33 +0530

    introduction to JPA and JDBC, introduction H2 database

---
+++ learn-jpa-hibernate/src/test/java/com/amrit/springboot/learnjpahibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrit.springboot.learnjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
