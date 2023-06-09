6124cede73040436f19fa26866e07ec848154f1e
Amrit Gaurav Rath <NH1965@nslhub.in>
2023-06-09 16:32:41 +0530

    Inserting queries using Spring JDBC

---
+++ learn-jpa-hibernate/src/main/java/com/amrit/springboot/learnjpahibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,25 @@
+package com.amrit.springboot.learnjpahibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class CourseJdbcRepository 
+{
+	@Autowired
+	private JdbcTemplate springJdbcTemplate;
+	
+	private static String INSERT_QUERY = 
+			
+			"""
+				insert into course (id, name, author)
+				values(1, 'Learn AWS','amrit');
+				
+			""";
+	public void insert()
+	{
+		springJdbcTemplate.update(INSERT_QUERY);
+	}
+}
