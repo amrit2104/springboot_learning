22ac67c77c50aa51a550303f18ab723d60c6cb3c
Amrit Gaurav Rath <NH1965@nslhub.in>
2023-06-12 14:51:17 +0530

    Various capabilities of Spring JPA and Jakarta Persistences.

--- learn-jpa-hibernate/src/main/java/com/amrit/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/amrit/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import com.amrit.springboot.learnjpahibernate.course.Course;
 import com.amrit.springboot.learnjpahibernate.course.jdbc.CourseJdbcRepository;
 import com.amrit.springboot.learnjpahibernate.course.jpa.CourseJpaRepository;
+import com.amrit.springboot.learnjpahibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner
@@ -14,18 +15,28 @@ public class CourseCommandLineRunner implements CommandLineRunner
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception 
 	{
-		repository.insert(new Course(1,"Learn JPA","Amrit"));
-		repository.insert(new Course(2,"Learn AWS","Gaurav"));
-		repository.insert(new Course(3,"Learn Azure","Rath"));
+		repository.save(new Course(1,"Learn JPA","Amrit"));
+		repository.save(new Course(2,"Learn AWS","Gaurav"));
+		repository.save(new Course(3,"Learn Azure","Rath"));
 		
-		repository.deleteById(2);
+		repository.deleteById(2l);
 		
-		System.out.println(repository.findById(1));
+		System.out.println(repository.findById(1l));
+		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		System.out.println(repository.findByAuthor("Amrit"));
+		System.out.println(repository.findByAuthor(""));
+		
+		System.out.println(repository.findByName("Learn Azure"));
 	}
 }
